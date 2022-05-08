c0d7f3e66a477f854d7887d4f6826e6a648a88d3
Yeesbae <Chrislim1234500@gmail.com>
2022-05-08 18:47:19 +0800

    Update AICustomer.cs

--- ProjectSustain/Assets/Scripts/Ai (customer)/AICustomer.cs
+++ ProjectSustain/Assets/Scripts/Ai (customer)/AICustomer.cs
@@ -31,39 +31,39 @@ public class AICustomer : MonoBehaviour
     {
         //Debug.Log("Test");
         
-        if (availableSeats > 0)
-        {
-            if (count == 1)
-            {
-                count--;
-                TableManager.Instance.GetAvailPosition(tableTransform);
+        //if (availableSeats > 0)
+        //{
+        //    if (count == 1)
+        //    {
+        //        count--;
+        //        TableManager.Instance.GetAvailPosition(tableTransform);
 
-                for (int i = 0; i < availableSeats; i++)
-                {
-                    //Debug.Log(TestManager.Instance.ListOfTables[i].availability);
-                    if (TableManager.Instance.ListOfTables[i].availability)
-                    {
-                        TableManager.Instance.ListOfTables[i].availability = false;
+        //        for (int i = 0; i < availableSeats; i++)
+        //        {
+        //            //Debug.Log(TestManager.Instance.ListOfTables[i].availability);
+        //            if (TableManager.Instance.ListOfTables[i].availability)
+        //            {
+        //                TableManager.Instance.ListOfTables[i].availability = false;
 
-                        Debug.Log("Agent " + i + 1 + " is going table " + TableManager.Instance.ListOfTables[i].tableName);
-                        Debug.Log(TableManager.Instance.ListOfTables[i].tableName + " is set to " + TableManager.Instance.ListOfTables[i].availability);
-                        //agent.SetDestination(tableTransform.transform.GetChild(i).position);
-                        count = 1;
+        //                Debug.Log("Agent " + i + 1 + " is going table " + TableManager.Instance.ListOfTables[i].tableName);
+        //                Debug.Log(TableManager.Instance.ListOfTables[i].tableName + " is set to " + TableManager.Instance.ListOfTables[i].availability);
+        //                //agent.SetDestination(tableTransform.transform.GetChild(i).position);
+        //                count = 1;
 
-                        return;
-                        //agent.SetDestination(gameObject.transform.GetChild(i).position);
-                        //Debug.Log("Table is taken: " + TestManager.Instance.ListOfTables[i].tableName);
-                        //Debug.Log(i);
-                        //Debug.Log(TestManager.Instance.ListOfTables[i].availability);
-                        //Debug.Log(gameObject.transform.GetChild(i).name);
-                    }
-                    else 
-                    {
-                        Debug.Log("Full house");
-                        // Wait outside restaurant
-                    }
-                }
-            }
-        }
+        //                return;
+        //                //agent.SetDestination(gameObject.transform.GetChild(i).position);
+        //                //Debug.Log("Table is taken: " + TestManager.Instance.ListOfTables[i].tableName);
+        //                //Debug.Log(i);
+        //                //Debug.Log(TestManager.Instance.ListOfTables[i].availability);
+        //                //Debug.Log(gameObject.transform.GetChild(i).name);
+        //            }
+        //            else 
+        //            {
+        //                Debug.Log("Full house");
+        //                // Wait outside restaurant
+        //            }
+        //        }
+        //    }
+        //}
     }
 }
