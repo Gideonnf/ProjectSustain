8a7547dd98a47a4166fa73029ed977cb1c2cfd54
Gideon Francis <46663395+Gideonnf@users.noreply.github.com>
2022-05-15 14:01:32 +0800

    Trash Can

---
+++ ProjectSustain/Assets/Scripts/Interactables/TrashInteractable.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashInteractable : InteractBase
+{
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    public override void Interact(GameObject playerObject)
+    {
+        playerReference = playerObject;
+        if (playerReference != null)
+        {
+            GameObject playerHands = playerReference.GetComponent<PlayerController>().playerHand;
+            // Has an ingredient in hand
+            if (playerReference.GetComponent<PlayerController>().ingredientObject)
+            {
+                GameObject ingredient = playerReference.GetComponent<PlayerController>().ingredientObject;
+                playerReference.GetComponent<PlayerController>().ingredientObject = null;
+
+                Destroy(ingredient);
+            }
+        }
+    }
+
+    public override void InteractEnd()
+    {
+        base.InteractEnd();
+    }
+}
